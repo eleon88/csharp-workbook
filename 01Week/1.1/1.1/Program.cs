e9613f6060043d6e4287224ec3f0e5028aba7594
ELEON <ELEON>
2017-04-24 21:03:55 -0500

    first day

---
+++ 01Week/1.1/1.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int result = number1 + number2;
+
+            Console.WriteLine("\n The result is " + result);
+
+            Console.WriteLine("\n please enter a number in yards to convert into inches");
+            int yards = int.Parse(Console.ReadLine());
+            int inches = yards * 36;
+
+            Console.WriteLine("\n" + yards + " yards = " + inches + " inches");
+
+            bool people = true;
+            bool f = false;
+            decimal num = 13;
+
+            Console.WriteLine("\n People = " + people + " f = " + f + " num = " + num * num);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01Week/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  DoStuff dostuff = new DoStuff();
+
+            Console.WriteLine("How old are you?");
+            int Age = int.Parse(Console.ReadLine());
+
+            string Greeting;
+            if (Age > 75)
+            {
+                Greeting = "You're old! Hello, Eric! you are " + Age.ToString() + " years old";
+            }
+            else
+            {
+                Greeting = "Hello, Eric you are " + Age.ToString() + " years old";
+            }
+
+            Console.WriteLine(Greeting);
+
+            Console.ReadLine();
+        }
+    }
+}
